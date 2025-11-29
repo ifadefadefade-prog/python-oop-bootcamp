ee43c9d4844d42aaad9efa89f3eaf9497f29493b
ifadefadefade-prog <i.fadefadefade@gmail.com>
2025-11-30 01:31:17 +0900

    Add files via upload

---
+++ lesson_1.py
@@ -0,0 +1,33 @@
+class Point:
+    color = 'red'
+    circle = 2
+    
+
+#я выпонял и прошлые все комнады из видео, просто в видео человека 
+#сказал что заново объявит класс и я закрыл терминал
+#The default interactive shell is now zsh.
+#To update your account to use zsh, please run `chsh -s /bin/zsh`.
+#For more details, please visit https://support.apple.com/kb/HT208050.
+#MacBook-Pro-fade:python-oop-bootcamp fadeaway$ python
+#bash: python: command not found
+#MacBook-Pro-fade:python-oop-bootcamp fadeaway$ 
+#MacBook-Pro-fade:python-oop-bootcamp fadeaway$ python3
+#Python 3.10.11 (v3.10.11:7d4cc5aa85, Apr  4 2023, 19:05:19) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+#Type "help", "copyright", "credits" or "license" for more information.
+#Cmd click to launch VS Code Native REPL
+#>>> class Point:
+#...     color = 'red'
+#...     circle = 2
+#...     
+#... 
+#>>> a = Point()
+#>>> b = Point()
+#>>> a.x = 1
+#>>> a.y = 2
+#>>> b.x = 10
+#>>> b.y = 20
+#>>> b.__dict__
+#{'x': 10, 'y': 20}
+#>>> a.__dict__
+#{'x': 1, 'y': 2}
+#>>> 
